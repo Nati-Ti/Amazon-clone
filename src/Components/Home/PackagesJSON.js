dbc72c2b717fcb4cfcd7ee2f7fde60128bc615ca
Nati-Ti <natnaelti94@gmail.com>
2023-11-07 00:49:11 +0300

    HomePage-update

---
+++ src/Components/Home/PackagesJSON.js
@@ -0,0 +1,46 @@
+
+const Packages = [
+    {
+        "PackageOfOne" : "true",
+        "Title" : "Small gifts under $20",
+        "Image" : "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2023/EBF23/Fuji_Desktop_Single_image_EBF_1x_v2._SY304_CB573698005_.jpg",
+        "Url" : "https://www.amazon.com/s/?_encoding=UTF8&k=gifts&rh=p_36%3A-2000&crid=2NI1HZ3R2RQLU&qid=1698772905&pd_rd_w=FK8HP&content-id=amzn1.sym.a2d9112d-b859-48c0-8be7-7e772c0d2430&pf_rd_p=a2d9112d-b859-48c0-8be7-7e772c0d2430&pf_rd_r=NSD8M90J9C9C4N759YSZ&pd_rd_wg=EsYVL&pd_rd_r=079938e5-f623-4c30-a3d3-5b7a20a115bd&ref_=pd_gw_unk"
+    },
+    {
+        "PackageOfOne" : "true",
+        "Title" : "Deals in PCs",
+        "Image" : "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2023/EBF23/Fuji_Desktop_Single_image_EBF_1x_v1._SY304_CB573698005_.jpg",
+        "Url" : "https://www.amazon.com/gp/goldbox/?ie=UTF8&ref_=nav_cs_gb&deals-widget=%257B%2522version%2522%253A1%252C%2522viewIndex%2522%253A0%252C%2522presetId%2522%253A%252207A70A321FD4643438052FBBE71BDE5D%2522%252C%2522departments%2522%253A%255B%2522541966%2522%255D%252C%2522sorting%2522%253A%2522FEATURED%2522%257D&pd_rd_w=rLBk7&content-id=amzn1.sym.912ecd74-9eeb-4579-95eb-3293710c1508&pf_rd_p=912ecd74-9eeb-4579-95eb-3293710c1508&pf_rd_r=NSD8M90J9C9C4N759YSZ&pd_rd_wg=EsYVL&pd_rd_r=079938e5-f623-4c30-a3d3-5b7a20a115bd"
+    },
+    {
+        "PackageOfOne" : "false",
+        "Title" : "Refresh your space",
+        PackagesOfFour : [ 
+            {
+                "Title1" : "Dining",
+                "Image1" : "https://images-na.ssl-images-amazon.com/images/G/01/launchpad/2023/Gateway/January/DesktopQuadCat_186x116_LP-HP_B08MYX5Q2W_01.23._SY116_CB619238939_.jpg"
+            },
+            {
+                "Title1" : "Home",
+                "Image1" : "https://images-na.ssl-images-amazon.com/images/G/01/launchpad/2023/Gateway/January/DesktopQuadCat_186x116_home_B08RCCP3HV_01.23._SY116_CB619238939_.jpg"
+            },
+            {
+                "Title1" : "Kitchen",
+                "Image1" : "https://images-na.ssl-images-amazon.com/images/G/01/launchpad/2023/Gateway/January/DesktopQuadCat_186x116_kitchen_B0126LMDFK_01.23._SY116_CB619238939_.jpg"
+            },
+            {
+                "Title1" : "Health and Beauty",
+                "Image1" : "https://images-na.ssl-images-amazon.com/images/G/01/launchpad/2023/Gateway/January/DesktopQuadCat_186x116_health-beauty_B07662GN57_01.23._SY116_CB619238939_.jpg"
+            }
+        ],
+        "Url" : "https://www.amazon.com/gp/goldbox/?ie=UTF8&ref_=nav_cs_gb&deals-widget=%257B%2522version%2522%253A1%252C%2522viewIndex%2522%253A0%252C%2522presetId%2522%253A%2522957D18D8132C0FCBA2EA0F57FEB9A3F8%2522%252C%2522departments%2522%253A%255B%25221055398%2522%255D%252C%2522sorting%2522%253A%2522BY_SCORE%2522%257D&pd_rd_w=IixvL&content-id=amzn1.sym.4b021efd-ffe2-474d-86fe-aea1d170c9ca&pf_rd_p=4b021efd-ffe2-474d-86fe-aea1d170c9ca&pf_rd_r=NSD8M90J9C9C4N759YSZ&pd_rd_wg=EsYVL&pd_rd_r=079938e5-f623-4c30-a3d3-5b7a20a115bd"
+    },
+    {
+        "PackageOfOne" : "true",
+        "Title" : "Toys under $25",
+        "Image" : "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2023/EBF23/Fuji_Desktop_Single_image_EBF_1x_v3._SY304_CB573698005_.jpg",
+        "Url" : "https://www.amazon.com/s/?_encoding=UTF8&k=toys&rh=p_36%3A-2500&pd_rd_w=H7RdW&content-id=amzn1.sym.8596ce56-6e6f-4ba4-b23a-4a5abe13dae6&pf_rd_p=8596ce56-6e6f-4ba4-b23a-4a5abe13dae6&pf_rd_r=NSD8M90J9C9C4N759YSZ&pd_rd_wg=EsYVL&pd_rd_r=079938e5-f623-4c30-a3d3-5b7a20a115bd&ref_=pd_gw_unk"
+    }
+]
+
+export default Packages;

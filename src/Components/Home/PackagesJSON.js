df90e1bf87902751199d7d6a07c37f1ef9f5cdb8
Nati-Ti <natnaelti94@gmail.com>
2023-11-07 22:17:17 +0300

    upadate

--- src/Components/Home/PackagesJSON.js
+++ src/Components/Home/PackagesJSON.js
@@ -13,26 +13,23 @@ const Packages = [
         "Url" : "https://www.amazon.com/gp/goldbox/?ie=UTF8&ref_=nav_cs_gb&deals-widget=%257B%2522version%2522%253A1%252C%2522viewIndex%2522%253A0%252C%2522presetId%2522%253A%252207A70A321FD4643438052FBBE71BDE5D%2522%252C%2522departments%2522%253A%255B%2522541966%2522%255D%252C%2522sorting%2522%253A%2522FEATURED%2522%257D&pd_rd_w=rLBk7&content-id=amzn1.sym.912ecd74-9eeb-4579-95eb-3293710c1508&pf_rd_p=912ecd74-9eeb-4579-95eb-3293710c1508&pf_rd_r=NSD8M90J9C9C4N759YSZ&pd_rd_wg=EsYVL&pd_rd_r=079938e5-f623-4c30-a3d3-5b7a20a115bd"
     },
     {
-        "PackageOfOne" : "false",
+        "PackageOfOne" : "",
         "Title" : "Refresh your space",
-        PackagesOfFour : [ 
+        "PackagesOfFour" :  
             {
                 "Title1" : "Dining",
-                "Image1" : "https://images-na.ssl-images-amazon.com/images/G/01/launchpad/2023/Gateway/January/DesktopQuadCat_186x116_LP-HP_B08MYX5Q2W_01.23._SY116_CB619238939_.jpg"
-            },
-            {
-                "Title1" : "Home",
-                "Image1" : "https://images-na.ssl-images-amazon.com/images/G/01/launchpad/2023/Gateway/January/DesktopQuadCat_186x116_home_B08RCCP3HV_01.23._SY116_CB619238939_.jpg"
-            },
-            {
-                "Title1" : "Kitchen",
-                "Image1" : "https://images-na.ssl-images-amazon.com/images/G/01/launchpad/2023/Gateway/January/DesktopQuadCat_186x116_kitchen_B0126LMDFK_01.23._SY116_CB619238939_.jpg"
-            },
-            {
-                "Title1" : "Health and Beauty",
-                "Image1" : "https://images-na.ssl-images-amazon.com/images/G/01/launchpad/2023/Gateway/January/DesktopQuadCat_186x116_health-beauty_B07662GN57_01.23._SY116_CB619238939_.jpg"
+                "Image1" : "https://images-na.ssl-images-amazon.com/images/G/01/launchpad/2023/Gateway/January/DesktopQuadCat_186x116_LP-HP_B08MYX5Q2W_01.23._SY116_CB619238939_.jpg",
+
+                "Title2" : "Home",
+                "Image2" : "https://images-na.ssl-images-amazon.com/images/G/01/launchpad/2023/Gateway/January/DesktopQuadCat_186x116_home_B08RCCP3HV_01.23._SY116_CB619238939_.jpg",
+
+                "Title3" : "Kitchen",
+                "Image3" : "https://images-na.ssl-images-amazon.com/images/G/01/launchpad/2023/Gateway/January/DesktopQuadCat_186x116_kitchen_B0126LMDFK_01.23._SY116_CB619238939_.jpg",
+            
+                "Title4" : "Health and Beauty",
+                "Image4" : "https://images-na.ssl-images-amazon.com/images/G/01/launchpad/2023/Gateway/January/DesktopQuadCat_186x116_health-beauty_B07662GN57_01.23._SY116_CB619238939_.jpg"
             }
-        ],
+        ,
         "Url" : "https://www.amazon.com/gp/goldbox/?ie=UTF8&ref_=nav_cs_gb&deals-widget=%257B%2522version%2522%253A1%252C%2522viewIndex%2522%253A0%252C%2522presetId%2522%253A%2522957D18D8132C0FCBA2EA0F57FEB9A3F8%2522%252C%2522departments%2522%253A%255B%25221055398%2522%255D%252C%2522sorting%2522%253A%2522BY_SCORE%2522%257D&pd_rd_w=IixvL&content-id=amzn1.sym.4b021efd-ffe2-474d-86fe-aea1d170c9ca&pf_rd_p=4b021efd-ffe2-474d-86fe-aea1d170c9ca&pf_rd_r=NSD8M90J9C9C4N759YSZ&pd_rd_wg=EsYVL&pd_rd_r=079938e5-f623-4c30-a3d3-5b7a20a115bd"
     },
     {
@@ -44,3 +41,24 @@ const Packages = [
 ]
 
 export default Packages;
+
+
+
+// "PackagesOfFour" : [ 
+//     {
+//         "Title1" : "Dining",
+//         "Image1" : "https://images-na.ssl-images-amazon.com/images/G/01/launchpad/2023/Gateway/January/DesktopQuadCat_186x116_LP-HP_B08MYX5Q2W_01.23._SY116_CB619238939_.jpg"
+//     },
+//     {
+//         "Title1" : "Home",
+//         "Image1" : "https://images-na.ssl-images-amazon.com/images/G/01/launchpad/2023/Gateway/January/DesktopQuadCat_186x116_home_B08RCCP3HV_01.23._SY116_CB619238939_.jpg"
+//     },
+//     {
+//         "Title1" : "Kitchen",
+//         "Image1" : "https://images-na.ssl-images-amazon.com/images/G/01/launchpad/2023/Gateway/January/DesktopQuadCat_186x116_kitchen_B0126LMDFK_01.23._SY116_CB619238939_.jpg"
+//     },
+//     {
+//         "Title1" : "Health and Beauty",
+//         "Image1" : "https://images-na.ssl-images-amazon.com/images/G/01/launchpad/2023/Gateway/January/DesktopQuadCat_186x116_health-beauty_B07662GN57_01.23._SY116_CB619238939_.jpg"
+//     }
+// ],
